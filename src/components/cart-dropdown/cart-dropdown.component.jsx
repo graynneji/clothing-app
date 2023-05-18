4cb4c5eefe4750ec5a2166924b7b99b1fd78a29a
Ukaegbu Gray <graynneji@outlook.com>
2023-05-18 15:36:19 +0200

    add to cart feauture

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,19 @@
 import "./cart-dropdown.style.scss";
 import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+  console.log(cartItems);
   return (
     <div className="cart-dropdown-container">
-      <div className="cart-item" />
-
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+      </div>
       <Button>CHECKOUT</Button>
     </div>
   );
